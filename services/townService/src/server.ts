c644126895a413026f2447e86cce105aeeb8136f
Vincenqwu <wuqiuan1997.qw@gmail.com>
2022-07-30 16:53:56 -0700

    add new route to the server

--- services/townService/src/server.ts
+++ services/townService/src/server.ts
@@ -4,11 +4,33 @@ import CORS from 'cors';
 import { AddressInfo } from 'net';
 import addTownRoutes from './router/towns';
 import CoveyTownsStore from './lib/CoveyTownsStore';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+const userRoute = require("./router/users");
+var bodyParser = require('body-parser')
+
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL!,
+).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.log(err);
+});
 
 const app = Express();
 app.use(CORS());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 const server = http.createServer(app);
 
+app.use('/users', userRoute);
 addTownRoutes(server, app);
 
 server.listen(process.env.PORT || 8081, () => {
