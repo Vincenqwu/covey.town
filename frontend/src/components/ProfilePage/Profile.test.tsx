c959ca6c0e218d3540788c15271063ae14e42fa2
cuitong100 <teresa.cui@hotmail.com>
2022-08-17 16:54:46 -0700

    fix lint

--- frontend/src/components/ProfilePage/Profile.test.tsx
+++ frontend/src/components/ProfilePage/Profile.test.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { RenderResult, waitFor , render, screen } from "@testing-library/react";
-import  userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom/extend-expect";
-import '@testing-library/jest-dom'
-import Profile from "./Profile";
-import Navbar from "./Profile";
 import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from "react-dom";
+import Profile from "./Profile";
 
 
 describe("Profile", () => {
-  let renderedComponent: RenderResult;
-//   let userNameTextField: HTMLInputElement;
-//   let passWordTextField: HTMLInputElement;
-  let editButton: HTMLElement;
-  let logoutButton: HTMLElement;
-  let deleteAcountButton: HTMLElement;
 
     it("renders without crashing", () => {
       const div = document.createElement("div");
